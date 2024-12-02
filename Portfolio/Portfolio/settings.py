3216ed57e312aef7fd86ed1cdccd0a2f5a5d24e7
AnastasiaSkachenko <skachenkoa@gmail.com>
2024-12-02 14:36:21 +0100

    secret key for development and debug

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -26,9 +26,9 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-0o5sz(!-ybirgz9h#2eu%b3qx8^!118n_8_0h%!+usy)2#ujeo')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'True')
 
 ALLOWED_HOSTS = []
 
